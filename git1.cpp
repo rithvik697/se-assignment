649430b326c92a824f3ad9d24319e63d66814300
Rithvik Maragoni <115717586+rithvik697@users.noreply.github.com>
2025-02-05 10:09:02 +0530

    Add files via upload

---
+++ git1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+// Function to calculate factorial using recursion
+long long factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    
+    if (num < 0) {
+        cout << "Factorial is not defined for negative numbers." << endl;
+    } else {
+        cout << "Factorial of " << num << " is " << factorial(num) << endl;
+    }
+    
+    return 0;
+}
